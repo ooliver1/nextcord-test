d281fed47d1e7092071b754b8c7e664cefefa611
Oliver Wilkes <oliverwilkes2006@icloud.com>
2022-06-05 19:52:23 +0100

    feat: test ext.application_checks.check_any (#75)

--- template3.py
+++ template3.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from botbase import CogBase
 from nextcord.ext.commands import Context
 
 from ._ import cmdname2
@@ -11,11 +10,10 @@ if TYPE_CHECKING:
     from nextcord_test.__main__ import MyBot
 
 
-class FileName(CogBase):
-    @cmdname2.group()
-    async def cmdname3(self, ctx: Context):
-        ...
+@cmdname2.group()
+async def cmdname3(ctx: Context):
+    ...
 
 
 def setup(bot: MyBot):
-    bot.add_cog(FileName(bot))
+    ...
